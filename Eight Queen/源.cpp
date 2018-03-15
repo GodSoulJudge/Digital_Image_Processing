0f0b3f7910ae2acc6e38acfbd2d96dcdb2bf2939
GodSoulJudge <37298293+GodSoulJudge@users.noreply.github.com>
2018-03-15 23:18:57 +0800

    Add files via upload

---
+++ Eight Queen/源.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<wchar.h>
+int v, i, j, k, l, s, a[99];
+void main() {
+	for (scanf("%d", &s); *a - s; v = a[j *= v] - a[i], k = i < s, j += (v = j < s && (!k && !!printf(2 + "\n\n%c" - (!l << !j), " #Q"[l^v ? (l^j) & 1 : 2]) &&
+		++l || a[i] < s&&v&&v - i + j && v + i - j)) && !(l %= s), v || (i == j ? a[i += k] = 0 : ++a[i]) >= s * k&&++a[--i]);
+	system("pause");
+}
